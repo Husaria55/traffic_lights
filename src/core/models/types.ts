22cce88810217d65b7d007f2a45f329451305760
Bartek <krikbart@gmail.com>
2026-04-17 09:11:48 +0200

    basic interfaces

---
+++ src/core/models/types.ts
@@ -0,0 +1,28 @@
+export typqe Road = "north" | "south" | "east" | "west";
+
+export interface AddVehicleCommand {
+    type: "addVehicle";
+    vehicleId: string;
+    startRoad: Road;
+    endRoad: Road;
+}
+
+export interface StepCommand {
+    type: "step";
+
+}
+
+
+export type Command = AddVehicleCommand | StepCommand;4
+
+export interface InputData {
+    commands: Command[];
+}
+
+export interface StepStatus {
+    leftVehicles: string[];
+}
+
+export interface OutputData {
+    stepStatuses: StepStatus[];
+}
